83daac9f73311b9e0f1f81c9cc2ecc7386c4af63
LikaLikona <likalikona@gmail.com>
2024-02-11 18:59:53 +0400

    3

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const API_URL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1&quot"
-const IMG_PATH = "https://image.tmdb.org/t/p/w1280&#39"
-const SEARCH_API = "https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="
+const API_URL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1"
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query='
 
 const main = document.getElementById('main')
 const form = document.getElementById('search')
@@ -20,10 +20,10 @@ function showMovies(movies){
         movieEl.classList.add("movie")
 
         movieEl.innerHTML = `
-            <img src="${poster_path}" alt="${title}>
+            <img src="${IMG_PATH + poster_path}" alt="${title}">
             <div class="movie_info">
                 <h3>${title}</h3>
-                <span class="${vote_average}"></span>
+                <span class="vote ${getClassByRate(vote_average)}">${vote_average}</span>
             </div>
             <div class = "overview">
                 ${overview}
@@ -31,4 +31,13 @@ function showMovies(movies){
         `
         main.appendChild(movieEl)
     })
+}
+function getClassByRate(vote){
+    if (vote >=7) {
+        return "green"
+    } else if (vote >=6) {
+        return "orange"
+    } else {
+        return "red"
+    }
 }
